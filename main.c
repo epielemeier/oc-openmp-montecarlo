ff8015dc4d4a5b3ebe83e4fd4f5327f2795cefd6
emilypie <empielemeier@gmail.com>
2017-11-14 21:42:11 -0600

    add parallel reduction

--- main.c
+++ main.c
@@ -2,8 +2,15 @@
 #ifndef _OPENMP
 #   include "omp.h"
 #endif
+#include <stdlib.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char* argv[]) {
+    long long int num_tosses = strtol(argv[1], NULL, 10);
+    printf("Number of tosses: %li\n", num_tosses);
+    long long int global_result = 0;
+#pragma omp parallel num_threads(num_tosses) \
+        reduction(+:global_result)
+    global_result += 1;
+    printf("Result: %li", global_result);
     return 0;
 }
