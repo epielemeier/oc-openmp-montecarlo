009d604d64c72c2fcbc572e45aaaaa7e9f556578
emilypie <empielemeier@gmail.com>
2017-11-14 22:00:13 -0600

    estimate pi with monte carlo

--- main.c
+++ main.c
@@ -4,13 +4,26 @@
 #endif
 #include <stdlib.h>
 
+int throw_dart();
+
 int main(int argc, char* argv[]) {
     long long int num_tosses = strtol(argv[1], NULL, 10);
     printf("Number of tosses: %li\n", num_tosses);
-    long long int global_result = 0;
+    long long int num_hits = 0;
 #pragma omp parallel num_threads(num_tosses) \
-        reduction(+:global_result)
-    global_result += 1;
-    printf("Result: %li", global_result);
+        reduction(+:num_hits)
+    num_hits += throw_dart();
+    double pi_estimate = 4 * num_hits / ((double) num_tosses);
+    printf("Result: %f", pi_estimate);
     return 0;
+}
+
+int throw_dart() {
+    double x = ((double)rand()) / RAND_MAX * 2 - 1;
+    double y = ((double)rand()) / RAND_MAX * 2 - 1;
+    double distance_squared = x*x + y*y;
+    if (distance_squared <= 1)
+        return 1;
+    else
+        return 0;
 }
